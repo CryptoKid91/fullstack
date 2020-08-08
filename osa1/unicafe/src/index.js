b72c50dc6ed46c0356f7f7d267a957024d45408a
Marco Stålfors <marco@stalfors.fi>
2020-08-09 00:30:28 +0300

    1.9

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -28,17 +28,25 @@ const App = () => {
 }
 
 const Statistics = ({ good, neutral, bad }) => {
-	return (
-		<div>
-			<h1>Statistics</h1>
-			<p>Good: {good}</p>
-			<p>Neutral: {neutral}</p>
-			<p>Bad: {bad}</p>
-			<p>All: {good + neutral + bad}</p>
-			<p>Average: {(good - bad) / (good + neutral + bad)}</p>
-			<p>Positive: {good / (good + neutral + bad) * 100} %</p>
-		</div>
-	)
+	if ((good + neutral + bad) !== 0) {
+		return (
+			<div>
+				<h1>Statistics</h1>
+				<p>Good: {good}</p>
+				<p>Neutral: {neutral}</p>
+				<p>Bad: {bad}</p>
+				<p>All: {good + neutral + bad}</p>
+				<p>Average: {(good - bad) / (good + neutral + bad)}</p>
+				<p>Positive: {good / (good + neutral + bad) * 100} %</p>
+			</div>
+		)
+	} else {
+		return (
+			<div>
+				<p>No feedback given</p>
+			</div>
+		)
+	}
 }
 
 
