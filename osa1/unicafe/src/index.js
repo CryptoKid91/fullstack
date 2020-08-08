29d72f335dbbc86888b6b133cecc9d38ad0eb43a
Marco Stålfors <marco@stalfors.fi>
2020-08-09 00:27:19 +0300

    1.7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -22,13 +22,25 @@ const App = () => {
 				handleClick={() => setBad(bad + 1)}
 				text='bad'
 			/>
+			<Statistics good={good} neutral={neutral} bad={bad} />
+		</div>
+	)
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+	return (
+		<div>
 			<h1>Statistics</h1>
 			<p>Good: {good}</p>
 			<p>Neutral: {neutral}</p>
 			<p>Bad: {bad}</p>
+			<p>All: {good + neutral + bad}</p>
+			<p>Average: {(good - bad) / (good + neutral + bad)}</p>
+			<p>Positive: {good / (good + neutral + bad) * 100} %</p>
 		</div>
 	)
 }
+
 
 const Button = ({ handleClick, text }) => (
 	<button onClick={handleClick}>
