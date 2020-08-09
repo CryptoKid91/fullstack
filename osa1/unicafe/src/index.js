f36c1de39460b93842d148791f2cd2d5c6b5bacf
Marco Stålfors <marco@stalfors.fi>
2020-08-09 10:15:49 +0300

    1.10

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -32,12 +32,12 @@ const Statistics = ({ good, neutral, bad }) => {
 		return (
 			<div>
 				<h1>Statistics</h1>
-				<p>Good: {good}</p>
-				<p>Neutral: {neutral}</p>
-				<p>Bad: {bad}</p>
-				<p>All: {good + neutral + bad}</p>
-				<p>Average: {(good - bad) / (good + neutral + bad)}</p>
-				<p>Positive: {good / (good + neutral + bad) * 100} %</p>
+				<StatisticLine text="Good" value={good} />
+				<StatisticLine text="Neutral" value={neutral} />
+				<StatisticLine text="Bad" value={bad} />
+				<StatisticLine text="All" value={good + neutral + bad} />
+				<StatisticLine text="Average" value={(good - bad) / (good + neutral + bad)} />
+				<StatisticLine text="Positive" value={good / (good + neutral + bad) * 100 + " %"} />
 			</div>
 		)
 	} else {
@@ -49,6 +49,7 @@ const Statistics = ({ good, neutral, bad }) => {
 	}
 }
 
+const StatisticLine = ({ text, value }) => <p>{text}: {value}</p>
 
 const Button = ({ handleClick, text }) => (
 	<button onClick={handleClick}>
