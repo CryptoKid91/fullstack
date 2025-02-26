16b74448b8bd263c93d4c963f414aad62db7772c
Marco Stålfors <mastal@utu.fi>
2025-02-26 10:12:27 +0200

    4.10

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,22 +1,33 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (req, res) => {
-	const blogs = await Blog.find({});
+	const blogs = await Blog.find({}).populate('user', { blogs: 0 });
 	res.json(blogs);
 });
 
 blogsRouter.post('/', async (req, res) => {
-	const blog = new Blog(req.body);
+	const { title, author, url, likes } = req.body;
+	const userRecord = await User.findById('67bdcd362f8bd71ab8389b4a');
+	const user = userRecord._id;
+	const blog = new Blog({ title, author, url, likes, user });
 
 	const result = await blog.save();
+	userRecord.blogs = userRecord.blogs.concat(result._id);
+	await userRecord.save();
+
 	res.status(201).json(result);
 });
 
 blogsRouter.delete('/:id', async (req, res) => {
-	const result = await Blog.findByIdAndDelete(req.params.id);
+	const id = req.params.id;
+	const result = await Blog.findByIdAndDelete(id);
 
 	if (result) {
+		const user = await User.findById(result.user);
+		console.log(user);
+		user.blogs = user.blogs.filter((b) => b !== id);
 		res.status(204).end();
 	} else {
 		res.status(404).end();
